c53553314b9bc70aaf49e2b57c0cf8b80f60d23c
maahi12491232 <maheshbabu.kosuri@.sasi.ac.in>
2024-01-26 14:16:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+import SignInForm from './SignInForm';
+
+const App = () => {
+  return (
+    <div>
+      <RegistrationForm />
+      <SignInForm />
+    </div>
+  );
+};
+
+export default App;
